f4478c374242002626b7344f07b5f5fc91aae75d
yarivg <yarivgavriel2@gmail.com>
2017-02-12 15:57:05 +0200

    Adding mashal

---
+++ hebChatbot/Entities/Mashal/Create/Run.py
@@ -0,0 +1,12 @@
+import webbrowser
+new = 2 # open in a new tab, if possible
+
+# open a public URL, in this case, the webbrowser docs
+url = "https://support.office.com/en-us/article/" \
+      "Create-an-Outlook-Data-File-pst-to-save-your-information-17a13ca2-df52-48e8-b933-4c84c2aabe7c"
+
+
+def run(dict, str_by_ref):
+    return
+    # str_by_ref[0] += '<a target="_blank" href=' + url + '>Pst in outlook</a>\n'
+    # webbrowser.open(url, new=new)
